0c3f545e5da8065b701ce380a08f6157e9c146f2
Florian Jenn <devel@effjot.net>
2020-08-30 08:25:10 +0200

    Test file for prefixes

---
+++ praefix-test.py
@@ -0,0 +1,27 @@
+import os
+import stiefo
+
+
+filenamebase = "praefix-test"
+
+wlist = stiefo.wordlist()
+
+wlist.load("Wortlisten/wortliste.txt")
+if os.path.isfile(filenamebase + '.wrd'):
+    wlist.load(filenamebase + '.wrd')
+
+with open(filenamebase + '.txt', "r", encoding="utf-8") as f:
+    text = f.read()
+
+
+stiefoWords, unknown = stiefo.convert_text(text, []) #[wlist])
+unk = stiefo.wordlist(unknown)
+unk.save(filenamebase + '.unk')
+
+print(unknown)
+
+st = stiefo.list_to_text(stiefoWords)
+with open(filenamebase + '.sti', "w", encoding="utf-8") as f:
+    f.write(st)
+
+stiefo.render_pdf(stiefoWords, filenamebase + ".pdf")
